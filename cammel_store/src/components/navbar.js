e98bac6f7d13e90b5fa9aec85d07663492fb5a2a
badeberner <bernerse@arcada.fi>
2024-02-14 13:38:26 +0200

    nav bar hella clean

--- cammel_store/src/components/navbar.js
+++ cammel_store/src/components/navbar.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/navbar.css'; // Adjusted import path
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import '../styles/navbar.css';
 
 const Navbar = () => {
-    return (
+  return (
       <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            Camel Store
-          </Link>
-          <ul className="navbar-menu">
-            <li className="navbar-item">
-              <Link to="/Login" className="navbar-links">Login</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/store" className="navbar-links">Store</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/cart" className="navbar-links">Cart</Link>
-            </li>
-            {/* Omit direct link to Checkout for better user flow */}
-          </ul>
-        </div>
+          <div className="navbar-container">
+              <Link to="/" className="navbar-logo">
+                  Camel Store
+              </Link>
+              <div className="search-bar">
+                  <input type="text" placeholder="Search..." />
+                  <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+              </div>
+              <ul className="navbar-menu">
+                  <li className="navbar-item">
+                      <Link to="/login" className="navbar-links">
+                          <FontAwesomeIcon icon={faUser} /> Login
+                      </Link>
+                  </li>
+                  <li className="navbar-item">
+                      <Link to="/store" className="navbar-links">Store</Link>
+                  </li>
+                  <li className="navbar-item">
+                      <Link to="/cart" className="navbar-links">
+                          <FontAwesomeIcon icon={faShoppingCart} /> Cart
+                      </Link>
+                  </li>
+              </ul>
+          </div>
       </nav>
-    );
-  };
-  
-  export default Navbar;
+  );
+};
+
+export default Navbar;
