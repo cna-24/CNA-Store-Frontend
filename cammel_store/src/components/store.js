27e97ae659390cca433e13427632d7a8a9e9f1a8
ahlerikm <ahlerikm@arcada.fi>
2024-03-13 10:27:20 +0200

    Product modal fixes

--- cammel_store/src/components/store.js
+++ cammel_store/src/components/store.js
@@ -11,6 +11,7 @@ const Store = () => {
   const [camels, setCamels] = useState([]);
   const token = process.env.REACT_APP_API_TOKEN; // Make sure this is defined in your environment variables
 
+  // Fetch data from the product API
   useEffect(() => {
     fetch(`${process.env.REACT_APP_PRODUCT_API}/products`)
       .then((res) => {
@@ -27,6 +28,8 @@ const Store = () => {
       });
   }, []);
 
+
+  // Load images from local folder
   const loadImage = async (productId) => {
     try {
       const { default: camelImageSrc } = await import(`../camelImages/${productId}.jpg`);
@@ -36,6 +39,7 @@ const Store = () => {
       return '';
     }
   };
+
 
   const CamelComponent = ({ camel }) => {
     const [modalOpen, setModalOpen] = useState(false);
@@ -75,6 +79,27 @@ const Store = () => {
     const openModal = () => setModalOpen(true);
     const closeModal = () => setModalOpen(false);
 
+    // Close modal if clicked outside of it
+    useEffect(() => {
+      function clickOutside(event) {
+          if (modalOpen) {
+              const modal = document.querySelector('.modal-content');
+
+              // Check if the click occurred outside the dropdown
+              if (modal && !modal.contains(event.target)) {
+                  setModalOpen(false);
+              }
+          }
+      }
+
+      // Add click event listener to the document
+      document.addEventListener('click', clickOutside);
+
+      // Cleanup function
+      return () => document.removeEventListener('click', clickOutside);
+    }, [modalOpen]);
+
+
     return (
       <>
         <div className="camel-card" onClick={openModal}>
@@ -85,7 +110,7 @@ const Store = () => {
             {camel.quantity > 0 ? (
               <div className='price'>
                 <h3>Price: {camel.price}€</h3>
-                <button onClick={handleAddToCartEnhanced} id='cartButton'><FontAwesomeIcon icon={faShoppingCart}/></button>
+                <button onClick={(event) => { event.stopPropagation(); handleAddToCartEnhanced(); }} id='cartButton'><FontAwesomeIcon icon={faShoppingCart}/></button>
               </div>
             ) : (
               <h3 className='stock'>Sold out!</h3>
@@ -96,18 +121,24 @@ const Store = () => {
         {modalOpen && (
           <div className="modal">
             <div className="modal-content">
+              <div>
+                <img src={imageSrc} alt={camel.name} className="modal-product-image" />
+              </div>
+
+              <div className='modal-text'>
+                <h2>{camel.name}</h2>
+                <p>{camel.description}</p>
+                {camel.quantity > 0 ? (
+                  <div className='modal-price'>
+                    <h3>Price: {camel.price}€</h3>
+                    <button onClick={handleAddToCartEnhanced} id='modalCartButton'><FontAwesomeIcon icon={faShoppingCart}/> Add to Cart</button>
+                  </div>
+                ) : (
+                  <h3 className='modalStock'>Sold out!</h3>
+                )}
+              </div>
+
               <span className="close" onClick={closeModal}>&times;</span>
-              <img src={imageSrc} alt={camel.name} className="modal-product-image" />
-              <h2>{camel.name}</h2>
-              <p>{camel.description}</p>
-              {camel.quantity > 0 ? (
-                <div className='modal-price'>
-                  <h3>Price: {camel.price}€</h3>
-                  <button onClick={handleAddToCartEnhanced} id='modalCartButton'><FontAwesomeIcon icon={faShoppingCart}/> Add to Cart</button>
-                </div>
-              ) : (
-                <h3 className='modalStock'>Sold out!</h3>
-              )}
             </div>
           </div>
         )}
@@ -156,4 +187,5 @@ const Store = () => {
     </div>
   );
 };
+
 export default Store;
